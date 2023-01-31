74b7f253e00940b77ade9554705267df5a3b309e
nick8319 <zhiltsov.nick@gmail.com>
2023-01-31 14:12:47 +0300

    feat: upgrade staging urls

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
-export const IFRAME_HOST = 'https://cronos-safe.3ona.co'
+export const IFRAME_HOST = 'https://cronos-safe.crolabs-int.co'
 export const IFRAME_PATH = '/migrate.html'
 export const MIGRATION_KEY = 'migrationFinished'
